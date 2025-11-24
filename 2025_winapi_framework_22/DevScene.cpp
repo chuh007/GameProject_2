e0faddb1f8973ff538d02ff43b7cef0af42b5cfc
박준서 <pppg12jintpt@gmail.com>
2025-11-24 17:33:57 +0900

    remove:commonEnemy temp

--- 2025_winapi_framework_22/DevScene.cpp
+++ 2025_winapi_framework_22/DevScene.cpp
@@ -20,12 +20,6 @@ void DevScene::Init()
 	Spawn<Boss>(Layer::ENEMY, { WINDOW_WIDTH / 2, WINDOW_HEIGHT / 4 }, { 100.f,100.f });
 	GET_SINGLE(CollisionManager)->CheckLayer(Layer::PROJECTILE, Layer::ENEMY);
 	GET_SINGLE(CollisionManager)->CheckLayer(Layer::PLAYER, Layer::DEFAULT);
-	Enemy* enemy = new Enemy;
-	enemy->SetSize({ 100.0f, 100.0f });
-	enemy->SetPos({ 200.0f, 0.0f });
-	GET_SINGLE(EnemySpawnManger)->SetSpawnScene(this);
-	GET_SINGLE(EnemySpawnManger)->AddEnemySpawnQueue({ 1.0f, enemy});
-	GET_SINGLE(EnemySpawnManger)->Init();
 	GET_SINGLE(ResourceManager)->Play(L"BGM");
 }
 
