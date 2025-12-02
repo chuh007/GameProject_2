a5af58f39286119ff4b1483fcf565ad6747df034
ggach16 <dlsdnj1234@gmail.com>
2025-12-02 12:20:19 +0900

    fixed : player hit state

---
+++ 2025_winapi_framework_22/PlayerDeadState.cpp
@@ -0,0 +1,14 @@
+﻿#include "pch.h"
+#include "PlayerDeadState.h"
+
+void PlayerDeadState::Enter(StateMachine* _fsm) {
+	std::cout << "Player is dead" << std::endl;
+}
+
+void PlayerDeadState::Excute(StateMachine* _fsm){
+
+}
+
+void PlayerDeadState::Exit(StateMachine* _fsm) {
+	// 타이틀로 씬 전환 시켜주기
+}
