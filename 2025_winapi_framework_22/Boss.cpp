388134ff64fe94d6f93a80d6dd7ce793f019a60b
chuh007 <chhuh007@gmail.com>
2025-12-08 16:19:14 +0900

    fix : PureBulletHellPattern

--- 2025_winapi_framework_22/Boss.cpp
+++ 2025_winapi_framework_22/Boss.cpp
@@ -43,12 +43,12 @@ Boss::Boss()
 	m_patternCompo->AddSpellPattern(3, spell3);
 	auto* pattern4 = new SpiralPattern(this, m_target, 1.75f, mover, L"");
 	m_patternCompo->AddNomalPattern(4, pattern4);
-	auto* spell4 = new PureBulletHellPattern(this, m_target, 1.5f, mover, L"사람을 죽이기 위한 순수한 탄막");
+	auto* spell4 = new PureBulletHellPattern(this, m_target, 3.f, mover, L"사람을 죽이기 위한 순수한 탄막");
 	m_patternCompo->AddSpellPattern(4, spell4);
 	auto* pattern5 = new SpiralPattern(this, m_target, 1.75f, mover, L"");
-	m_patternCompo->AddNomalPattern(4, pattern5);
+	m_patternCompo->AddNomalPattern(5, pattern5);
 	auto* spell5 = new PureBulletHellPattern(this, m_target, 1.5f, mover, L"제 2마법「」");
-	m_patternCompo->AddSpellPattern(4, spell5);
+	m_patternCompo->AddSpellPattern(5, spell5);
 
 	m_patternCompo->UseNomalPattern();
 }
--- 2025_winapi_framework_22/PureBulletHellPattern.cpp
+++ 2025_winapi_framework_22/PureBulletHellPattern.cpp
@@ -1,11 +1,12 @@
 #include "pch.h"
 #include "PureBulletHellPattern.h"
+#include "EnemyProjectile.h"
 
 PureBulletHellPattern::PureBulletHellPattern(Object* _owner, Object* _target, float _patternUseTime, BossMover* _mover, wstring _name)
     : Pattern(_owner, _target, _patternUseTime, _mover, _name)
 	, m_projectileType(PoolType::EnemyProjectile)
-	, m_fireCount(30)
-	, m_speed(225)
+	, m_fireCount(50)
+	, m_speed(150)
 {
 }
 
@@ -20,8 +21,8 @@ void PureBulletHellPattern::Update()
 	if (m_curTime > m_BaseShoutCooldown)
 	{
 		m_curTime = 0;
-		int x = (rand() % GAME_WIDTH / 2) + GAME_WIDTH / 4;
-		int y = (GAME_HEIGHT / 5) + rand() % 100;
+		int x = (rand() % 300) + GAME_WIDTH / 2 - 150;
+		int y = (GAME_HEIGHT / 4);
 		m_mover->MoveTo({ x,y }, m_BaseShoutCooldown);
 		BaseShoot();
 	}
@@ -29,4 +30,16 @@ void PureBulletHellPattern::Update()
 
 void PureBulletHellPattern::BaseShoot()
 {
+	float angle = 360.f / (float)m_fireCount;
+	GET_SINGLE(ResourceManager)->Play(L"FireSound");
+	for (int i = 0; i < m_fireCount; ++i)
+	{
+		auto* projectile = PoolManager::GetInst()->
+			Pop<EnemyProjectile>(PoolType::EnemyProjectile);
+		projectile->SetSize({ 55.f, 55.f });
+		projectile->SetColliderSize(27.5f);
+		projectile->SetPos(m_owner->GetPos());
+		projectile->SetDir(angle * i);
+		projectile->SetSpeed(m_speed);
+	}
 }
