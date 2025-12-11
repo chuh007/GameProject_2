824752f2f64c74d27f201d4b04aea20104cdb5f2
ggach16 <dlsdnj1234@gmail.com>
2025-12-11 09:43:32 +0900

    added : enemy 3 in resourceManager

--- 2025_winapi_framework_22/ResourceManager.cpp
+++ 2025_winapi_framework_22/ResourceManager.cpp
@@ -221,6 +221,7 @@ void ResourceManager::RegisterTexture()
 	LoadTexture(L"NormalEnemy", L"Texture\\enemy_1.bmp");
 	LoadTexture(L"DownEnemy", L"Texture\\Enemy_Down.bmp");
 	LoadTexture(L"Enemy_2", L"Texture\\Enemy_2.bmp");
+	LoadTexture(L"Enemy_3", L"Texture\\Enemy_3.bmp");
 }
 
 void ResourceManager::LoadTexture(const wstring& _key, const wstring& _path)
